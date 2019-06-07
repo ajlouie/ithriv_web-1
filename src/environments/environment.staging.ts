978c8667ec9684b1ad8f15e43c09765da8020faa
Ravi Chamakuri <rkc7h@virginia.edu>
2019-06-07 16:20:11 -0400

    Pushing change to handle uat web setup

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   envName: 'staging',
   production: true,
-  api: 'https://staging-ithriv.uvasomrc.io',
+  api: 'http://dev.ithriv.org',
   ga_tracking_id: 'UA-138286052-1'
 };
