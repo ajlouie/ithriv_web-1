93ee9137170706bc7e6a184ce366924834ad7492
Aaron Louie <ajlouie@gmail.com>
2021-02-16 11:43:23 -0500

    Adds unit test for getIrbInvestigators

--- src/app/shared/commons-api/commons-api.service.spec.ts
+++ src/app/shared/commons-api/commons-api.service.spec.ts
@@ -1,12 +1,46 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Dataset } from '../../commons-types';
+import { User } from '../../user';
+import { mockDataset } from '../fixtures/dataset';
+import { mockIrbInvestigators } from '../fixtures/investigators';
+import { mockUser } from '../fixtures/user';
 import { CommonsApiService } from './commons-api.service';
 
 describe('CommonsApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpMock: HttpTestingController;
+  let service: CommonsApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        CommonsApiService,
+      ]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(CommonsApiService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: CommonsApiService = TestBed.get(CommonsApiService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get list of IRB Investigators for a dataset', () => {
+    service.getDatasetIrbInvestigators(mockUser, mockDataset).subscribe(data => {
+      expect(data).toEqual(mockIrbInvestigators);
+    });
+
+    const req = httpMock.expectOne(`undefined/commons/meta/datasets/undefined/investigators`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockIrbInvestigators);
   });
 });
