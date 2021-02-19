cc211dccfcfbeed27adf5c14669eb9a736033e2a
Ravi Chamakuri <rkc7h@virginia.edu>
2021-02-19 02:55:18 -0500

    Pushing env config

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,9 +1,11 @@
+const servicehost = 'uat.ithriv.org';
+
 export const environment = {
   envName: 'staging',
   production: true,
-  api: 'https://uat.ithriv.org',
+  api: `https://${servicehost}`,
   ga_tracking_id: 'UA-138286052-1',
-  api_commons_adapter: `http://${localhost}/commons_adapter/api/`,
-  api_commons_adapter_private: `http://${localhost}/commons_adapter/api/private/`,
+  api_commons_adapter: `https://${servicehost}/commons_adapter/api/`,
+  api_commons_adapter_private: `https://${servicehost}/commons_adapter/api/private/`,
   landing_service: [{ name: 'UVA', url: 'https://poc.commons.ithriv.org' }],
 };
