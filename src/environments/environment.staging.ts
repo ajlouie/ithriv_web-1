298d2d3b6426e25ea565e4c6f4175a99a3342107
Ravi Chamakuri <rkc7h@virginia.edu>
2021-02-19 02:46:10 -0500

    Pushing env config changes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,8 @@ export const environment = {
   envName: 'staging',
   production: true,
   api: 'https://uat.ithriv.org',
-  ga_tracking_id: 'UA-138286052-1'
+  ga_tracking_id: 'UA-138286052-1',
+  api_commons_adapter: `http://${localhost}/commons_adapter/api/`,
+  api_commons_adapter_private: `http://${localhost}/commons_adapter/api/private/`,
+  landing_service: [{ name: 'UVA', url: 'https://poc.commons.ithriv.org' }],
 };
