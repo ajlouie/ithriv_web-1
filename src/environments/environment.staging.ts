4b2a49f266219b1cfece9cd81bc7d50317ae8f0d
Ravi Chamakuri <rkc7h@virginia.edu>
2021-02-22 23:01:51 -0500

    Pushing changes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -7,5 +7,5 @@ export const environment = {
   ga_tracking_id: 'UA-138286052-1',
   api_commons_adapter: `https://${servicehost}/commons_adapter/api/`,
   api_commons_adapter_private: `https://${servicehost}/commons_adapter/api/private/`,
-  landing_service: [{ name: 'UVA', url: 'https://${servicehost}' }],
+  landing_service: [{ name: 'UVA', url: `https://${servicehost}` }],
 };
