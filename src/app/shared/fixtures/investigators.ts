a728693c40364db3ce0baadcad8abc2b6839c9ad
Aaron Louie <ajlouie@gmail.com>
2021-02-24 14:29:56 -0500

    Removes investigator type & type labels

--- src/app/shared/fixtures/investigators.ts
+++ src/app/shared/fixtures/investigators.ts
@@ -1,24 +1,14 @@
-import { IrbInvestigatorType, IrbInvestigatorTypeLabel } from '../../commons-types';
-
 export const mockIrbInvestigators = [
   {
     email: 'jjl4d@virginia.edu',
-    type: IrbInvestigatorType.PI,
-    type_full: IrbInvestigatorTypeLabel.PI,
   },
   {
     email: 'gsw2c@virginia.edu',
-    type: IrbInvestigatorType.DEPT_CH,
-    type_full: IrbInvestigatorTypeLabel.DEPT_CH,
   },
   {
     email: 'rkc7h@virginia.edu',
-    type: IrbInvestigatorType.SC_I,
-    type_full: IrbInvestigatorTypeLabel.SC_I,
   },
   {
     email: 'ajl2j@virginia.edu',
-    type: IrbInvestigatorType.AS_C,
-    type_full: IrbInvestigatorTypeLabel.AS_C,
   },
 ];
